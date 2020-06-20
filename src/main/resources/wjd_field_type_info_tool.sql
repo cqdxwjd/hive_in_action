caea08c4dda6a11875b41f576e2dccc6ee7ce3d4
王京东 <wangjingdong@MacBook-Pro.local>
2020-06-20 11:41:54 +0800

    a

--- src/main/resources/wjd_field_type_info_tool.sql
+++ src/main/resources/wjd_field_type_info_tool.sql
@@ -1,18 +1,10 @@
-SELECT eq_id,
-       dya,
-       dyb,
-       dyc,
-       dla,
-       dlb,
-       dlc,
-       NULL,
-       NULL,
-       kzms,
-       gzz,
-       ldl,
-       1,
-       error,
-       NULL,
-       upd_time
-FROM dsep.stg_light_equipment_status
+SELECT relation_id,
+       media_id,
+       media_name,
+       media_type,
+       create_human_id,
+       create_time,
+       concat(media_path, '/', coalesce(media_uploaded_name, media_name))
+FROM dsep.stg_to_media
+WHERE date_format(create_time,"yyyy-MM-dd") = "2018-08-16" -- 取昨天产生的数据
 LIMIT 1
