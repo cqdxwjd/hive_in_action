d8ffd7871132633f364e48828e108a073fffaa91
shishuihuo <cqdxwjd@gmail.com>
2021-11-09 10:20:50 +0800

    sql打卡 20211109

--- src/main/resources/sql_training/每日一题.sql
+++ src/main/resources/sql_training/每日一题.sql
@@ -38,3 +38,52 @@ from
                            on a.Id = b.DepartmentId
     ) c
 where c.rn = 1;
+
+--2.寻找连续出现的数字
+
+create table Logs(
+                     Id bigint,
+                     Num bigint
+);
+
+insert into table Logs values
+    (1,1),
+    (2,1),
+    (3,1),
+    (4,2),
+    (5,1),
+    (6,2),
+    (7,2);
+
+--方式1，使用窗口函数求差值
+select
+    a.Num as ConsecutiveNums
+from
+    (
+        select
+            Id as Id,
+            Num as Num,
+            row_number() over (partition by Num order by Id) as rn
+        from Logs
+    ) a
+group by
+        a.Id-a.rn,--如果是连续出现的数字，那么根据数字分组，在组内排序之后得到的序号与原始序号差值相等。
+        a.Num
+having count(*) >= 3 --过滤至少出现3次的数字。
+;
+
+--方式2，连续join自身2次
+select
+    distinct ConsecutiveNums
+from(
+        select
+            t1.Num as Num1,
+            t2.Num as Num2,
+            t3.Num as Num3,
+            if(t1.Num=t2.Num=t3.Num,t1.Num,null) as ConsecutiveNums
+        from Logs t1
+                 left join Logs t2 on t1.Id = t2.Id - 1
+                 left join Logs t3 on t2.Id = t3.Id - 1
+    ) a
+where a.ConsecutiveNums is not null
+;
